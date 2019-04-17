3222c65b144e3d2466c3dd155c5c81be2067c3a9
Federico Monti <federico.monti@molo17.com>
2019-04-17 09:28:21 +0200

    Integrated Cloud Firestore with read/write operation

--- app/src/main/java/com/fm/federico/authfirestorepoc/MainActivity.kt
+++ app/src/main/java/com/fm/federico/authfirestorepoc/MainActivity.kt
@@ -14,12 +14,37 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        user_role_text_view.text = "You are successful logged in"
+        getUserRoleWithQuery()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         FirebaseAuth.getInstance().signOut()
+    }
+
+    private fun getUserRoleWithQuery() {
+        currentUser?.uid?.let { uid ->
+            firestore
+                .collection("roles")
+                .whereArrayContains("users", uid)
+                .get()
+                .addOnCompleteListener { task ->
+                    task.result?.run {
+                        when (documents.size) {
+                            1 -> {
+                                val role = documents[0].reference.path.split("/")[1]
+                                user_role_text_view.text = "Your role is: $role"
+                            }
+                            else -> showError()
+                        }
+                    }
+                }
+                .addOnFailureListener { showError() }
+        } ?: showError()
+    }
+
+    private fun showError() {
+        user_role_text_view.text = "Role not found for this user"
     }
 
     companion object {
